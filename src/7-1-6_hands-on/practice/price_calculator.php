e86ff3b2d48058aff246da0d7ef918c21732cd07
Keiko Inoue <k1218148@gmail.com>
2026-06-20 14:20:55 +0900

    復習１回目

--- src/7-1-6_hands-on/practice/price_calculator.php
+++ src/7-1-6_hands-on/practice/price_calculator.php
@@ -6,29 +6,24 @@
     <title>Document</title>
 </head>
 <body>
-<?php
+    <?php
 
 
-$product_name = "ノートパソコン";
-$price = 80000; // 商品単価
-$quantity = 2; // 購入個数
-$tax_rate = 0.1; // 消費税率（10%)
+    $product_name ="ノートパソコン";
+    $price = 80000;
+    $quantity = 2;
+    $tax_rate = 0.1;
 
-
-//計算処理
-$subtotal = $price * $quantity; // 小計160000円
-$tax_amount = $subtotal * $tax_rate; // 消費税 16000円
-$total = $subtotal + $tax_amount; // 合計金額 176000円
-
-// 結果の表示
-echo "商品名 : " . $product_name . "<br>"; //ノートパソコンが出力される
-echo"単価 : " . $price . "円" . "<br>"; //80000円が出力される
-echo "数量 : " . $quantity . "個" . "<br>"; //2が出力される
-echo "小計：" . $subtotal . "円" . "<br>"; //160000円が出力される
-echo "消費税(10%)：" . $tax_amount . "円<br>"; //16000円が出力される
-echo "<strong>合計金額：" . $total . "円</strong><br>"; //176000円が出力される
-
-?>
+    $subtotal = $price * $quantity ; // 小計を計算
+    $tax_amount = $subtotal * $tax_rate; // 消費税率を計算
+    $total = $subtotal + $tax_amount; // 税込金額を計算
+    
+    echo "商品名：" .$product_name;// 商品名：ノートパソコンと出力される
+    echo "<br>単価：" . number_format($price) . "円"; // 単価：80000円と出力される
+    echo "<br>数量：" .number_format ($quantity) . "個"; // 数量：2個と出力される
+    echo "<br>小計：" . number_format ($subtotal) . "円";  //小計：160000円と出力される
+    echo "<br>消費税(10%)：" . number_format ($tax_amount) . "円"; // 消費税（10％）：16000円と出力される
+    echo "<br><strong>合計金額：" . number_format ($total) . "円</strong>"; // 合計金額：176000円と出力される
+    ?>
 </body>
-
-</html>
+</html>()
